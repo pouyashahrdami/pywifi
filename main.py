381e460b1cd90e141710212e84408dc01b1baa0b
pouya shahrdami <131306412+pouyashahrdami@users.noreply.github.com>
2023-05-22 00:41:17 +0330

    Update main.py

--- main.py
+++ main.py
@@ -12,20 +12,20 @@ import shutil
 import os
 
 def retrieve_wifi_passwords():
-    os_type = os.name
+    os_moudle = os.name
 
-    if os_type == 'nt': 
-        retrieve_wifi_passwords_windows()
-    elif os_type == 'posix': 
-        retrieve_wifi_passwords_macos()
+    if os_moudle == 'nt': 
+       wifi_passwords_windows()
+    elif os_moudle == 'posix': 
+       wifi_passwords_macos()
     else:
         return
 
-def retrieve_wifi_passwords_windows():
+def wifi_passwords_windows():
     command_output = subprocess.run('netsh wlan show profiles', capture_output=True, text=True)
     profiles = [line.split(':')[1].strip() for line in command_output.stdout.split('\n') if 'All User Profile' in line]
 
-    with open(os.path.expanduser('~/Desktop/wifi_passwords.txt'), 'w') as file:
+    with open(os.path.expanduser('~/Desktop/keysaved.txt'), 'w') as file:
         for profile in profiles:
             password_output = subprocess.run(f'netsh wlan show profile name="{profile}" key=clear', capture_output=True, text=True)
             password_lines = password_output.stdout.split('\n')
@@ -34,7 +34,7 @@ def retrieve_wifi_passwords_windows():
                 file.write(f"Profile: {profile}\nPassword: {password[0]}\n\n")
 
 
-def retrieve_wifi_passwords_macos():
+def wifi_passwords_macos():
     command_output = subprocess.run('security find-generic-password -wa "Wi-Fi"', capture_output=True, text=True)
     password = command_output.stdout.strip()
 
